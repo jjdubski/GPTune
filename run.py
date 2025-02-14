92c8241b2e68fcf7a4e5b30a3c13039773716106
jjdubski <101855602+jjdubski@users.noreply.github.com>
2025-02-14 16:30:51 -0600

    fix run.py and added colors

--- run.py
+++ run.py
@@ -2,13 +2,24 @@ import subprocess
 import sys
 import os
 
+def check_command(command):
+    try:
+        subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        return True
+    except FileNotFoundError:
+        print(f"\t\033[91mCommand not found: {command[0]}\033[0m")
+        return False
+    except subprocess.CalledProcessError:
+        return False
+
 try:
     # First check if node modules are installed
     if not os.path.exists('frontend/node_modules'):
         os.makedirs('frontend/node_modules')
 
     # Delete node_modules
-    print('Deleting existing node_modules directory...')
+    print('\033[93mDeleting node_modules directory...\033[0m')
+    print('\033[93mDeleting package-lock.json file...\033[0m')
     if os.name == 'posix':
         subprocess.run(['rm', '-rf', 'frontend/node_modules'])
         subprocess.run(['rm', 'frontend/package-lock.json'])
@@ -16,21 +27,28 @@ try:
         subprocess.run(['rmdir', '/s', '/q', 'frontend\\node_modules'], shell=True)
         subprocess.run(['del', '/q', 'frontend\\package-lock.json'], shell=True)
 
-    print('Installing node_modules...')
-    subprocess.run(['npm', 'install'], cwd='frontend', shell=True)
-    print('Node modules installed.')
-    print('Starting app with docker compose...')
-    try:
-        subprocess.run(['docker-compose', 'up', '--build'], check=True, shell=True)
-    except subprocess.CalledProcessError:
+    print('\033[92mInstalling node_modules...\033[0m')
+    subprocess.run(['npm', 'install'], cwd='frontend')
+    print('\033[92mNode modules installed.\033[0m')
+    print('Starting app with\033[94m docker-compose\033[0m...')
+
+    if check_command(['docker-compose', 'version']):
         try:
-            subprocess.run(['docker', 'compose', 'up', '--build'], check=True, shell=True)
-        except subprocess.CalledProcessError:
-            print("Docker compose up failed, make sure it's running")
-            sys.exit(1)
+            subprocess.run(['docker-compose', 'up', '--build'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"\033[91mError running 'docker-compose': {e}\033[0m")
+    elif check_command(['docker', 'compose', 'version']):
+        print('Starting app with\033[94m docker composed\033[0m instead...')
+        try:
+            subprocess.run(['docker', 'compose', 'up', '--build'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"\033[91mError running 'docker compose': {e}\033[0m")
+    else:
+        print("\033[93mNeither 'docker compose' nor 'docker-compose' commands are available.\033[0m")
+        print("\033[93m\tMake sure Docker is installed and running.\033[0m")
 
 except KeyboardInterrupt:
-    print("\nExiting...")
+    print("\n\033[91mExiting...\033[0m")
 
-print('Bye-bye :)')
-print('Run docker compose down -v to shutdown the containers and remove volumes')
+print("\033[93mBye-bye!\033[0m")
+print('Run\033[94m docker compose down -v\033[0m to remove container + volumes (including DB data)')
