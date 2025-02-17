ae59fbd844090e7c97cfcd19bfed9ecdc5a0a2fb
Matteo McCreedy <mccreedym1738@gmail.com>
2025-02-17 08:13:39 -0600

    added code for playlist component, still has bugs

--- frontend/src/components/Playlist/Playlist.tsx
+++ frontend/src/components/Playlist/Playlist.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'; 
 import './Playlist.css';
+import Song from '../Song/Song.tsx';
 
+
+//dev links:
+//https://react.dev/reference/react/useEffect
+//https://react.dev/reference/react/useState 
+//
 
 interface PlaylistProps {
-    title: string;
+    pname: string;
     img : string;
 }
 
-const Playlist: React.FC<PlaylistProps> = ({title, img}) => {
+const Playlist: React.FC<PlaylistProps> = ({pname, img}) => {
+    const [songs, setSongs] = useState<Song[]>([]);
     const image = 'https://seeded-session-images.scdn.co/v1/img/track/6KXxcGWj6KB5GlW1c2dhY5/en';
+    
+    useEffect(() => {
+        fetch('http://localhost:8000/songAPI/songs/?format=json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network error: Failed to connect to localhost:8000');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Data success:', data);
+                setSongs(data);
+            });
+    }, []);
+    
     return (
-        <div className='playlist'>
-            <img src={image} alt={`${title}`} />
-            <div className='playlist-info'>
-                <p>{title || 'Unknown'}</p>
-            </div>
-           
+        <div className="playlistElement">
+            {songs.length > 0 ? ( //note to self: this is a loop. just looks weird.
+                songs.map((song) => ( //map each song to each element
+                    <div key={song.id} className="playlist"> 
+                        <img 
+                            src='https://seeded-session-images.scdn.co/v1/img/track/6KXxcGWj6KB5GlW1c2dhY5/en'
+                            alt={song.title}  //alt in case link explodes
+                        />
+                        <div className="playlist-info"> 
+                            <p><strong>{song.title}</strong></p>
+                            <p>{song.artist}</p>
+                            <p>{song.album}</p>
+                            <p>{song.img}</p>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p className="playlist-info">Playlist is empty. What, do you not listen to music?</p>
+            )}
         </div>
     );
 };
