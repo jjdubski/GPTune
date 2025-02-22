7914caf19ddb210d66ce577537e86ffb643b6f67
Matteo <mccreedym1738@gmail.com>
2025-02-22 15:32:33 -0600

    added image for refresh

--- frontend/src/components/RefreshIcon/RefreshIcon.tsx
+++ frontend/src/components/RefreshIcon/RefreshIcon.tsx
@@ -7,7 +7,7 @@ interface RefreshButtonProps {
   const RefreshButton: React.FC<RefreshButtonProps> = ({ onRefresh }) => {
     return (
       <button className="refresh-button" onClick={onRefresh}>
-        <img src="/refresh-icon.png" alt="Refresh" className="refresh-icon" />
+        <img src="/refreshicon.png" alt="Refresh" className="refresh-icon" />
       </button>
     );
   };
