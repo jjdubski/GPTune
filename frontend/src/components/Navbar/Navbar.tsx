d4255feac9895cf2215d4ad8e05009e5b52d7bd3
John Lathrop <lathrop1009@gmail.com>
2025-02-23 22:36:02 -0600

    Fix Navbar link closing tag and comment out Playlists.css import because it doesnt exist

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar: React.FC = () => {
         <h1>Vite + React</h1>
         <nav>
           <Link to="/">Home</Link> | <Link to="/songs">Songs</Link> | <Link to="/playlist">Playlist</Link> | <Link to ="http://127.0.0.1:8000/login/">Login</Link>      
-          <Link to="/spotify"><SpotifyButton /><Link>
+          <Link to="/spotify"><SpotifyButton /></Link>
     </nav>
   </header>
     );
