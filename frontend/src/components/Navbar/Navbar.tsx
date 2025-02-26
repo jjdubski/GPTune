e254839eb34bc12afa3166eb0cfb580e8800d58d
YFAR02 <yfarooqui001@gmail.com>
2025-02-25 19:20:20 -0600

    Search page

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from '../../assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './Navbar.css'; // Assuming you have some CSS for styling
 import SpotifyButton from '../SpotifyButton/SpotifyButton';
 
@@ -10,14 +10,14 @@ const Navbar: React.FC = () => {
 
     return (
         <header className='App-header'>
-        <img src={viteLogo} className='logo' alt='Vite logo' />
-        <img src={reactLogo} className='logo react' alt='React logo' />
-        <h1>Vite + React</h1>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/songs">Songs</Link> | <Link to="/playlist">Playlist</Link> | <Link to ="http://127.0.0.1:8000/login/">Login</Link>      
-          <Link to="/spotify"><SpotifyButton /><Link>
-    </nav>
-  </header>
+            {/* <img src={viteLogo} className='logo' alt='Vite logo' />
+            <img src={reactLogo} className='logo react' alt='React logo' />
+            <h1>Vite + React</h1> */}
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/songs">Songs</Link> | <Link to="/playlists">Playlist</Link> | <Link to="http://127.0.0.1:8000/login/">Login</Link>      
+                <Link to="/spotify"><SpotifyButton title="Spotify" img="spotify-logo.png" /></Link>
+            </nav>
+        </header>
     );
 };
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,73 +1,116 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import './Home.css'
-import HomeTile from '../components/HomeTile/HomeTile'
-import SideMenu from '../components/SideMenu/SideMenu'
-import SpotifyButton from '../components/SpotifyButton/SpotifyButton'
-import './Home.css'
+import React, { useState, useEffect } from 'react';
+import { Link} from 'react-router-dom';
+import HomeTile from '../components/HomeTile/HomeTile';
+import User from '../components/User/User';
+import SpotifyButton from '../components/SpotifyButton/SpotifyButton';
+import SearchBar from '../components/SearchBar/SearchBar';
+import './Home.css';
 
-
-// interface HomeProps {
-//   currentDate: number
-//   currentTime: number
-// }
-
-// const Home: React.FC<HomeProps> = ({ currentDate, currentTime }) => {
 const Home: React.FC = () => {
-    const [currentTime, setCurrentTime] = useState('')
-    const [currentDate, setCurrentDate] = useState('')
-    const [currentUser, setCurrentUser] = useState('')
+    const [response, setResponse] = useState('');
+    const [currentUser, setCurrentUser] = useState<{ email: string, username: string, image: string }>({ email: '', username: '', image: '' });
 
-    // Fetches date and time from backend on load/reload
-    // useEffect(() => {
-    //   fetch('http://localhost:8000').then(res => res.json()).then(data => {
-    //     setCurrentTime(data.current_time)
-    //     setCurrentDate(data.current_date)
-    //   })
-    // }
+    const handleGenerateResponse = async () => {
+        const requestData = {
+            prompt: 'give me a random color',
+            num_runs: 1
+        };
 
-    // Updates date and time every second
-    useEffect(() => {
-        const fetchData = () => {
-            fetch('http://127.0.0.1:8000').then(res => res.json()).then(data => {
-                setCurrentTime(data.current_time)
-                setCurrentDate(data.current_date)
-                setCurrentUser(data.user)
-            })
+        try {
+            const res = await fetch('http://127.0.0.1:8000/generate_response/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestData)
+            });
+
+            const data = await res.json();
+            if (res.ok) {
+                setResponse(data.response);
+            } else {
+                console.error('Error:', data.error);
+            }
+        } catch (error) {
+            console.error('Error:', error);
         }
-
-        fetchData()
-        const interval = setInterval(fetchData, 1000)
-
-        return () => clearInterval(interval)
+    };
+    function findSongs(query: string) {
+        // console.log(query);
+        window.location.href = `/search`;
+        // jake needs to fix this
+        // const generateResponse = async () =>{
+        //     const requestData = {
+        //         prompt: query,
+        //         num_runs: 1
+        //     };
+        //     try {
+        //         const res = await fetch('http://127.0.0.1:8000/generate_response/', {
+        //             method: 'POST',
+        //             headers: {
+        //                 'Content-Type': 'application/json'
+        //             },
+        //             body: JSON.stringify(requestData)
+        //         });
+    
+        //         const data = await res.json();
+        //         if (res.ok) {
+        //             setResponse(data.response);
+        //         } else {
+        //             console.error('Error:', data.error);
+        //         }
+        //     } catch (error) {
+        //         console.error('Error:', error);
+        //     }
+        // }
     }
-    ,[])
+
+    
+    useEffect(() => {
+        fetch('http://localhost:8000').then(res => res.json()).then(data => {
+            setCurrentUser({
+                email: data.user.email || '',
+                username: data.user.display_name || '',
+                image: data.user.image || '/spotify-logo.png'
+            });
+            console.log(data);
+        });
+        handleGenerateResponse();
+    }, []);
+
     return (
         <div className="home-container">
-            {/* Side Menu */}
-            <SideMenu />
+            <SearchBar onSearch={findSongs}/>
             <div>
-                <h2>Home Page</h2>
-                <p>The date is  {currentDate} and the time is {currentTime}.</p>
-                <p>Logged in as: {currentUser.email}</p>
+                {currentUser.email ? (
+                    <User username={currentUser.username} image={currentUser.image} />
+                ) : (
+                    <div className="spotify-button-container">
+                        <SpotifyButton
+                            title="Link Spotify"
+                            img="./SpotifyButton.png"
+                        />
+                    </div>
+                )}
 
-                <div className="spotify-button-container">
-                    <SpotifyButton 
-                        title="Link Spotify"
-                        img="./SpotifyButton.png"
-                    />
-                </div>
-
-
-                
                 <div className="music-home-container">
-                    <HomeTile title="Discover" img = "/Discover.png" />  
-                    <HomeTile title="Add to Playlist" img="./AddtoPlaylist.png" />
-                    <HomeTile title="This or That?" img="./ThisorThat.png" />
+                    <Link to="/discover">
+                        <HomeTile title="Discover" img="/Discover.png" />
+                    </Link>
+                    <Link to="/add-to-playlist">
+                        <HomeTile title="Add to Playlist" img="./AddtoPlaylist.png" />
+                    </Link>
+                    <Link to="/this-or-that">
+                        <HomeTile title="This or That?" img="./ThisorThat.png" />
+                    </Link>
+                </div>
+                <div>
+                    <h3>Generated Response:</h3>
+                    <p>{response}</p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
