98961192cf5b9e92db85bb0acfb5f9e3a2405177
YFAR02 <yfarooqui001@gmail.com>
2025-03-03 16:49:00 -0600

    updating song component

--- frontend/src/components/AddSong/AddSong.tsx
+++ frontend/src/components/AddSong/AddSong.tsx
@@ -1,39 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "./AddSong.css";
-import Song from "../Song/Song";
-import plusIcon from "/plus-icon.png"
+import plusIcon from "/plus-icon.png";
 
 interface Song {
   title: string;
   artist: string;
   album: string;
   image: string;
+  trackURI: string; 
+  onPlay: (trackURI: string) => void;
 }
 
 const AddSong: React.FC<{ song: Song }> = ({ song }) => {
   const [isLoading, setIsLoading] = useState(true);
-  
+
   useEffect(() => {
     const img = new Image();
     img.src = song.image;
     img.onload = () => setIsLoading(false);
-  }
-  , [song]);
-  
-  return (
-    isLoading ? (
-      <></>
-    ) :
+  }, [song]);
+
+  return isLoading ? 
+  <></>
+ : (
     <div className="add-song">
-      <img className="song-image" src={song.image} alt="cover_art"/>
-      <div className="song-info">
-      <p className="song-title">{song.title}</p>
-      <p className="song-artist">{song.artist}</p>
-      <p className="song-album">{song.album}</p>
-      </div>
-      <button className="add-icon-button" onClick={() => console.log("Add song clicked")}>
-      <img src={plusIcon} alt="plus-icon" className="add-icon" />
+      <button 
+        className="song-image-container"
+        onClick={() => song.onPlay(song.trackURI)} 
+      >
+        <div className="arrow-right"></div> 
+
+        <img className="song-image" src={song.image} alt="cover_art" />
       </button>
+
+      <div className="song-info">
+        <p className="song-title">{song.title}</p>
+        <p className="song-artist">{song.artist}</p>
+        <p className="song-album">{song.album}</p>
+      </div>
+
+      {/* Add to Playlist Button */}
+      <img src={plusIcon} alt="plus-icon" className="add-icon" />
     </div>
   );
 };
