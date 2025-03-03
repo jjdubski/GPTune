2458f9bd8471ce14309a580be1d7b2bcbab78e2d
jjdubski <101855602+jjdubski@users.noreply.github.com>
2025-03-03 00:48:30 -0600

    fix home (remove debug code)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,8 +15,8 @@ import './Home.css';
 const Home: React.FC = () => {
     // const [currentTime, setCurrentTime] = useState('')
     // const [currentDate, setCurrentDate] = useState('')
+    // const [response, setResponse] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [response, setResponse] = useState([]);
     const [currentUser, setCurrentUser] = 
         useState<{email: string, username: string, image: string}>({email: '', username: '', image: ''});
 
@@ -46,35 +46,35 @@ const Home: React.FC = () => {
     // }
     // ,[])
 
-    const handleGenerateResponse = async () => {
-        return // to stop prompting for now
-        const requestData = {
-            prompt: 'give me a song to listen on a long drive',
-            num_runs: 5
-        };
-        try {
-            const res = await fetch('http://127.0.0.1:8000/getRecommendations/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(requestData)
-            });
+    // const handleGenerateResponse = async () => {
+    //     return // to stop prompting for now
+    //     const requestData = {
+    //         prompt: 'give me a song to listen on a long drive',
+    //         num_runs: 5
+    //     };
+    //     try {
+    //         const res = await fetch('http://127.0.0.1:8000/getRecommendations/', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify(requestData)
+    //         });
 
-            const data = await res.json();
-            if (res.ok) {
-                // var output = "";
-                // output+= data.response.title 
-                // output+= data.response.artist
-                // output+= data.response.album
-                setResponse(data.response);
-            } else {
-                console.error('Error:', data.error);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+    //         const data = await res.json();
+    //         if (res.ok) {
+    //             // var output = "";
+    //             // output+= data.response.title 
+    //             // output+= data.response.artist
+    //             // output+= data.response.album
+    //             setResponse(data.response);
+    //         } else {
+    //             console.error('Error:', data.error);
+    //         }
+    //     } catch (error) {
+    //         console.error('Error:', error);
+    //     }
+    // };
     function findSongs(query: string) {
         // console.log(query);
         window.location.href = `/search`;
@@ -117,7 +117,7 @@ const Home: React.FC = () => {
             console.log("User:", data);
             setIsLoading(false);
         });
-        handleGenerateResponse();
+        // handleGenerateResponse();
     }, []);
 
     return (
@@ -146,8 +146,8 @@ const Home: React.FC = () => {
                     </Link>
                 </div>
                 <div>
-                    <h3>Generated Response:</h3>
-                    <p>{response}</p>
+                    {/* <h3>Generated Response:</h3>
+                    <p>{response}</p> */}
                 </div>
                 {/* <p>The date is  {currentDate} and the time is {currentTime}.</p> */}
                 {/* <p>Logged in as: {currentUser.email}</p> */}
