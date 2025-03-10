8888c5f778da9f9b3b390e73041596c1b22ef543
Matteo <mccreedym1738@gmail.com>
2025-03-09 20:11:53 -0500

    requested changes

--- frontend/src/components/TitleBubble/TitleBubble.tsx
+++ frontend/src/components/TitleBubble/TitleBubble.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./TitleBubble.css"; // Import the CSS file
 
 interface TitleBubbleProps {
   text: string;
@@ -6,9 +7,9 @@ interface TitleBubbleProps {
 
 const TitleBubble: React.FC<TitleBubbleProps> = ({ text }) => {
   return (
-    <div className="inline-flex items-center px-4 py-1 bg-green-400 rounded-full border-2 border-black shadow-lg relative">
-      <span className="text-black font-bold tracking-widest">{text}</span>
-      <div className="absolute -top-1 left-0 right-0 h-1 border-t-2 border-dashed border-black"></div>
+    <div className="title-bubble">
+      <span className="title-text">{text}</span>
+      <div className="title-border"></div>
     </div>
   );
 };
